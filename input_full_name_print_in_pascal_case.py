32077e18074611fd24b73c9da5e362fc238b3dc2
Linuun <jlenonninolla31@gmail.com>
2025-03-19 23:32:44 +0800

    print the input in pascal case

--- input_full_name_print_in_pascal_case.py
+++ input_full_name_print_in_pascal_case.py
@@ -1,3 +1,4 @@
 # ask user to input their full name
 full_name = input("Enter your full name: ")
 # print the input in pascal case
+print("Your name in pascal case is", '' .join(word.capitalize() for word in full_name.split()))
