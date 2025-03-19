42b2c75b6e0cbf56d4f422823979562258bee2e5
Linuun <jlenonninolla31@gmail.com>
2025-03-19 22:42:40 +0800

    fixed a mistake in pseudocode

--- input_full_name_with_spaces_print_without_spaces.py
+++ input_full_name_with_spaces_print_without_spaces.py
@@ -1,4 +1,4 @@
 # ask user to input their full name with space characters at the beginning
 full_name = input("Enter your full name (put spaces at the beginning): ")
-# print the output with no spaces at the beginning
+# print the input with no spaces at the beginning
 print("Your full name is", full_name.lstrip())
