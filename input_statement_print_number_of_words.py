1ea424cc83c7229b48d8a3ae17f3405c8bd3768b
Linuun <jlenonninolla31@gmail.com>
2025-03-19 23:10:31 +0800

    print the number of words in the statement

--- input_statement_print_number_of_words.py
+++ input_statement_print_number_of_words.py
@@ -1,3 +1,4 @@
 # ask the user to input a complete statement
 complete_statement = input("Enter a complete statement: ")
 # print the number of words in the statement
+print("The number of words in the statement is", len(complete_statement.split()))
