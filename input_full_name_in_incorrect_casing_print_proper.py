a28e47ba6ba55e6f7477d239670019cd57a89fde
Linuun <jlenonninolla31@gmail.com>
2025-03-19 22:54:22 +0800

    print the input in its proper casing

--- input_full_name_in_incorrect_casing_print_proper.py
+++ input_full_name_in_incorrect_casing_print_proper.py
@@ -1,3 +1,4 @@
 # ask user to input their full name in incorrect casing
 full_name = input("Enter your full name (in incorrect casing): ")
 # print the input in its proper casing
+print("Your name is", full_name.title())
