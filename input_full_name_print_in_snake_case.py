e7683912ca04932244c6101b7a400b3f6ee1d6a3
Linuun <jlenonninolla31@gmail.com>
2025-03-19 23:39:52 +0800

    print the input in snake case

--- input_full_name_print_in_snake_case.py
+++ input_full_name_print_in_snake_case.py
@@ -1,3 +1,4 @@
 # ask user to input their full name in incorrect casing
 full_name = input("Enter your full name (in incorrect casing): ")
 # print the input in snake case
+print("Your name in snake case is", '_' .join(word.lower() for word in full_name.split()))
