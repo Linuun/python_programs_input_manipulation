9a769ae32739de0c60c87268a0654364ad1cfed0
Linuun <jlenonninolla31@gmail.com>
2025-03-19 23:37:01 +0800

    added some words in pseudocode and user input

--- input_full_name_print_in_pascal_case.py
+++ input_full_name_print_in_pascal_case.py
@@ -1,4 +1,4 @@
-# ask user to input their full name
-full_name = input("Enter your full name: ")
+# ask user to input their full name in incorrect casing
+full_name = input("Enter your full name (in incorrect casing): ")
 # print the input in pascal case
 print("Your name in pascal case is", '' .join(word.capitalize() for word in full_name.split()))
