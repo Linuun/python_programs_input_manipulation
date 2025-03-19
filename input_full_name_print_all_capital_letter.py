3b0d8a6440681d981209b61b21164e881a61b418
Linuun <jlenonninolla31@gmail.com>
2025-03-19 22:43:35 +0800

    fixed a mistake in pseudocode

--- input_full_name_print_all_capital_letter.py
+++ input_full_name_print_all_capital_letter.py
@@ -1,4 +1,4 @@
 # ask user to input their full name 
 full_name = input("Enter your full name: ")
-# print the output in all capital letter
+# print the input in all capital letter
 print("Your full name is", full_name.upper())
