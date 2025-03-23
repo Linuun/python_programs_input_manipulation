6836df43d9e2ba4d399a2742e814e3fde79efccf
Linuun <jlenonninolla31@gmail.com>
2025-03-23 22:43:36 +0800

    changed f-string into zfill function

--- input_number_from_0_to_1000_print_in_6_digit_format.py
+++ input_number_from_0_to_1000_print_in_6_digit_format.py
@@ -1,4 +1,4 @@
 # ask user to input a number from 0 to 1000
 number = int(input("Enter a number (0-1000): "))
 # print the output in 6-digit format
-print(f"{number:06}")
+print(str(number).zfill(6))
